68681de6a5686ac104e463c9b0aa2420eafacd2a
mvttn <85101143+mvttn@users.noreply.github.com>
2023-12-22 10:55:00 +1100

    Add price discrepancy finder

---
+++ data_collection/price_discrepancy.py
@@ -0,0 +1,25 @@
+import json
+
+def compare_prices(api_files):
+    # Create a dictionary to store prices for each API
+    api_prices = {}
+
+    # Load data from files
+    for api_file in api_files:
+        with open(api_file, 'r') as file:
+            api_prices[api_file] = {key: float(value) for key, value in (line.strip().strip(',').split(':') for line in file)}
+
+    # Extract the list of cryptocurrencies
+    cryptocurrencies = set().union(*(api.keys() for api in api_prices.values()))
+
+    # Compare prices for each cryptocurrency
+    for coin in cryptocurrencies:
+        prices = [api_prices[api].get(coin, None) for api in api_files]
+        if all(prices):  # Check if the coin is present in all APIs
+            discrepancies = [prices[i] - prices[i - 1] for i in range(1, len(prices))]
+            print(f"{coin}: Prices - {', '.join(map(str, prices))}, Discrepancies - {', '.join(map(str, discrepancies))}")
+
+if __name__ == "__main__":
+    api_files = ["coinmarketcap.txt", "coingecko.txt", 'coinmarketcap.txt']  
+
+    compare_prices(api_files)
